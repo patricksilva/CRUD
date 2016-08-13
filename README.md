9c43aedb21083daec00fcbfb72d68325d84d12ff
Patrick Alex <patrickalex@gmail.com>
2016-08-13 16:12:53 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Objective
+- Develop a basic CRUD project using the Java language.
+Course: Academia do Java, VOFFICE
+Author: Patrick Alex - patrickalex@gmail.com
+Date: 2016-08-13
---
+++ src/br/com/globalcode/ajtf96/patrick/Crud.java
@@ -0,0 +1,21 @@
+package br.com.globalcode.ajtf96.patrick;
+
+public class Crud {
+	
+	public void create() {
+		//TODO
+	}
+	
+	public void read() {
+		//TODO
+	}
+	
+	public void update() {
+		//TODO
+	}
+	
+	public void delete() {
+		//TODO
+	}
+
+}
