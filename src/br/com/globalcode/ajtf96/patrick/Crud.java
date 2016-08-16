3d251e38cecc359433da0960e8c6fb81f0ff09f1
Patrick Alex <patrickalex@gmail.com>
2016-08-16 03:33:28 -0300

    Adds java.sql imports. Adds private static final Strings for connection parameters

--- src/br/com/globalcode/ajtf96/patrick/Crud.java
+++ src/br/com/globalcode/ajtf96/patrick/Crud.java
@@ -1,6 +1,23 @@
 package br.com.globalcode.ajtf96.patrick;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ * 
+ * @author Patrick Alex - patrickalex@gmail.com
+ * CRUD is a Database Patterns designed to organize the persistence operations 
+ * of an application into Create, Retrieve, Update, and Delete operations that
+ * are implemented by a persistence layer.
+ * 
+ * */
 public class Crud {
+	
+	private static final String NOME_BANCO = "aj";
+	private static final String STR_CON = "jdbc:mysql://localhost:3306/" + NOME_BANCO;
+	private static final String USER = "aj";
+	private static final String PASSWORD = "ajtf96%";
 	
 	public void create() {
 		//TODO
